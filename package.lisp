f978b5ed8ebc4d41c08e881fc952a9f4f509d27f
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-02-04 13:45:41 -0800

    export unsafe pixel functions

--- package.lisp
+++ package.lisp
@@ -9,57 +9,57 @@
 
    #:1-bit-gray-image
    #:make-1-bit-gray-image
-   #:1-bit-gray-pixel
+   #:1-bit-gray-pixel #:1-bit-gray-pixel*
 
    #:2-bit-gray-image
    #:make-2-bit-gray-image
-   #:2-bit-gray-pixel
+   #:2-bit-gray-pixel #:2-bit-gray-pixel*
 
    #:4-bit-gray-image
    #:make-4-bit-gray-image
-   #:4-bit-gray-pixel
+   #:4-bit-gray-pixel #:4-bit-gray-pixel*
 
    #:8-bit-gray-image
    #:make-8-bit-gray-image
-   #:8-bit-gray-pixel
+   #:8-bit-gray-pixel #:8-bit-gray-pixel*
 
    #:16-bit-gray-image
    #:make-16-bit-gray-image
-   #:16-bit-gray-pixel
+   #:16-bit-gray-pixel #:16-bit-gray-pixel*
 
 
    #:4-bit-rgb-image
    #:make-4-bit-rgb-image
-   #:4-bit-rgb-pixel
+   #:4-bit-rgb-pixel #:4-bit-rgb-pixel*
 
    #:5-bit-rgb-image
    #:make-5-bit-rgb-image
-   #:5-bit-rgb-pixel
+   #:5-bit-rgb-pixel #:5-bit-rgb-pixel*
 
    #:8-bit-rgb-image
    #:make-8-bit-rgb-image
-   #:8-bit-rgb-pixel
+   #:8-bit-rgb-pixel #:8-bit-rgb-pixel*
 
    #:16-bit-rgb-image
    #:make-16-bit-rgb-image
-   #:16-bit-rgb-pixel
+   #:16-bit-rgb-pixel #:16-bit-rgb-pixel*
 
 
    #:4-bit-rgba-image
    #:make-4-bit-rgba-image
-   #:4-bit-rgba-pixel
+   #:4-bit-rgba-pixel #:4-bit-rgba-pixel*
 
    #:5-bit-rgba-image
    #:make-5-bit-rgba-image
-   #:5-bit-rgba-pixel
+   #:5-bit-rgba-pixel #:5-bit-rgba-pixel*
 
    #:8-bit-rgba-image
    #:make-8-bit-rgba-image
-   #:8-bit-rgba-pixel
+   #:8-bit-rgba-pixel #:8-bit-rgba-pixel*
 
    #:16-bit-rgba-image
    #:make-16-bit-rgba-image
-   #:16-bit-rgba-pixel
+   #:16-bit-rgba-pixel #:16-bit-rgba-pixel*
 
    #:read-jpeg-file
    #:read-jpeg-stream
