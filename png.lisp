52bb6fa185ef9e85f3c2ffb813ef4567be0c75c9
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-02-06 19:39:35 -0800

    add (8-bit anyway) PNG image writing support

--- png.lisp
+++ png.lisp
@@ -29,7 +29,7 @@
             ((and (eq colour-type :truecolor-alpha)
                   (eql bit-depth 8))
              (let ((img (make-8-bit-rgba-image height width)))
-               (declare (type 8-bit-rgb-image img))
+               (declare (type 8-bit-rgba-image img))
                (loop for i below height
                   do 
                     (loop for j below width
@@ -61,7 +61,7 @@
             ((and (eq colour-type :greyscale)
                   (eql bit-depth 8))
              (let ((img (make-8-bit-gray-image height width)))
-               (declare (type 8-bit-rgb-image img))
+               (declare (type 8-bit-gray-image img))
                (loop for i below height
                   do 
                     (loop for j below width
@@ -76,3 +76,68 @@
 (defun read-png-file (pathname)
   (with-open-file (stream pathname :direction :input :element-type '(unsigned-byte 8))
     (read-png-stream stream)))
+
+
+(defun write-png-stream (stream image)
+  (typecase image
+    (8-bit-rgb-image
+     (with-image-bounds (ymax xmax)
+         image
+       (let ((displaced
+              (make-array (* ymax xmax 3)
+                          :element-type '(unsigned-byte 8)
+                          :initial-contents
+                          (make-array (* ymax xmax 3)
+                                      :element-type '(unsigned-byte 8)
+                                      :displaced-to (transpose-image image)))))
+         (zpng:write-png-stream 
+          (make-instance 'zpng:png
+                         :color-type :truecolor
+                         :height xmax
+                         :width ymax
+                         :bpp 8
+                         :image-data displaced)
+          stream))))
+    (8-bit-rgba-image
+     (with-image-bounds (ymax xmax)
+         image
+       (let ((displaced
+              (make-array (* ymax xmax 3)
+                          :element-type '(unsigned-byte 8)
+                          :initial-contents
+                          (make-array (* ymax xmax 4)
+                                      :element-type '(unsigned-byte 8)
+                                      :displaced-to (transpose-image image)))))
+         (zpng:write-png-stream 
+          (make-instance 'zpng:png
+                         :color-type :truecolor-alpha
+                         :height xmax
+                         :width ymax
+                         :bpp 8
+                         :image-data displaced)
+          stream))))
+    (8-bit-gray-image
+     (with-image-bounds (ymax xmax)
+         image
+       (let ((displaced
+              (make-array (* ymax xmax)
+                          :element-type '(unsigned-byte 8)
+                          :initial-contents
+                          (make-array (* ymax xmax)
+                                      :element-type '(unsigned-byte 8)
+                                      :displaced-to (transpose-image image)))))
+         (zpng:write-png-stream 
+          (make-instance 'zpng:png
+                         :color-type :grayscale
+                         :height xmax
+                         :width ymax
+                         :bpp 8
+                         :image-data displaced)
+          stream))))
+    (t (error "No PNG writing support for this image type."))))
+
+(defun write-png-file (pathname image)
+  (with-open-file (stream pathname :direction :output
+                          :if-exists :supersede
+                          :element-type '(unsigned-byte 8))
+    (write-png-stream stream image)))
