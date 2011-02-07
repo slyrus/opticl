53e7858d86b20c0e6f1f8ec372253477a1b04ca2
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-02-06 18:49:41 -0800

    update tests

--- test/opticl-test.lisp
+++ test/opticl-test.lisp
@@ -19,12 +19,18 @@
                                   '("opticl-test" "test")
                                   :initial-value nil))))
 
+(defun tiff-image (filename)
+  (reduce #'merge-pathnames (list filename "images/")
+          :from-end t
+          :initial-value (asdf:component-pathname
+                          (asdf:find-system "retrospectiff"))))
+
 (ensure-directories-exist (output-image ""))
 
 (defun test-tiff-read-8-bit-gray ()
   (let* ((file (test-image "truck-gray.tiff"))
          (img (read-tiff-file file)))
-    (let ((out (output-image "truck-gray.png")))
+    (let ((out (output-image "truck-gray.jpeg")))
       (write-jpeg-file out img))))
 
 (defun test-tiff-read-8-bit-rgb ()
@@ -58,3 +64,27 @@
     (let ((out (output-image "truck.jpeg")))
       (write-jpeg-file out img))))
 
+
+(defun test-snow ()
+  (let* ((file (tiff-image "snow.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "snow-rgba.jpeg")))
+      (write-jpeg-file out img))))
+
+(defun test-snow-lzw ()
+  (let* ((file (tiff-image "snow-lzw.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "snow-lzw.jpeg")))
+      (write-jpeg-file out img))))
+
+(defun test-snow-rgb ()
+  (let* ((file (tiff-image "snow-rgb.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "snow-rgb.jpeg")))
+      (write-jpeg-file out img))))
+
+(defun test-snow-packbits ()
+  (let* ((file (tiff-image "snow-packbits.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "snow-packbits.jpeg")))
+      (write-jpeg-file out img))))
