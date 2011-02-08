4597199fa44c42cc0a6810da939841f1783538c0
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-02-07 21:08:29 -0700

    added gamma functions and copy-image

--- imageops.lisp
+++ imageops.lisp
@@ -14,3 +14,14 @@
                  do (setf (pixel zimg j i) (pixel img i j))))
       zimg)))
 
+(defun copy-image (img)
+  (with-image-bounds (ymax xmax channels)
+      img
+    (let ((zimg (make-array
+                 (cons ymax (cons xmax (when channels (list channels))))
+                 :element-type (array-element-type img))))
+      (loop for i below ymax
+         do (loop for j below xmax
+               do (setf (pixel zimg i j) (pixel img i j))))
+      zimg)))
+
