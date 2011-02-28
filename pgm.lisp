261a4ce7dcad4d7b6f0204d69559ed583fef147c
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-02-28 15:41:20 -0800

    add writing PGM ASCII files and refactor PGM reading code a bit

--- pgm.lisp
+++ pgm.lisp
@@ -26,30 +26,55 @@
     (map 'string #'code-char
          (cons first-char (read-until-whitespace stream)))))
 
+(defun %read-pgm-ascii-stream (stream)
+  (let ((width (parse-integer (read-number stream)))
+        (height (parse-integer (read-number stream)))
+        (maxgray (parse-integer (read-number stream))))
+    (if (< maxgray 256)
+        (let ((img (make-8-bit-gray-image height width)))    
+          (loop for i below height
+             do 
+             (loop for j below width
+                do (setf (pixel img i j)
+                         (read-number stream))))
+          img)
+        (let ((img (make-8-bit-gray-image height width)))    
+          (loop for i below height
+             do 
+             (loop for j below width
+                do (setf (pixel img i j)
+                         (read-number stream))))
+          img))))
+
+(defun %read-pgm-binary-stream (stream)
+  (let ((width (parse-integer (read-number stream)))
+        (height (parse-integer (read-number stream)))
+        (maxgray (parse-integer (read-number stream))))
+    (if (< maxgray 256)
+        (let ((img (make-8-bit-gray-image height width)))    
+          (loop for i below height
+             do 
+             (loop for j below width
+                do (setf (pixel img i j)
+                         (read-byte stream))))
+          img)
+        (let ((img (make-8-bit-gray-image height width)))    
+          (loop for i below height
+             do 
+             (loop for j below width
+                do (setf (pixel img i j)
+                         (+ (ash (read-byte stream) 8)
+                            (read-byte stream)))))
+          img))))
+
 (defun read-pgm-stream (stream)
   (let ((magic (make-array 2 :element-type '(unsigned-byte 8))))
     (read-sequence magic stream)
-    (if (equalp magic #(80 53)) ;; P5 magic number
-        (let ((width (parse-integer (read-number stream)))
-              (height (parse-integer (read-number stream)))
-              (maxgray (parse-integer (read-number stream))))
-          (if (< maxgray 256)
-              (let ((img (make-8-bit-gray-image height width)))    
-                (loop for i below height
-                   do 
-                     (loop for j below width
-                        do (setf (pixel img i j)
-                                 (read-byte stream))))
-                img)
-              (let ((img (make-8-bit-gray-image height width)))    
-                (loop for i below height
-                   do 
-                     (loop for j below width
-                        do (setf (pixel img i j)
-                                 (+ (ash (read-byte stream) 8)
-                                    (read-byte stream)))))
-                img)))
-        (error "Invalid PGM Magic Number"))))
+    (cond ((equalp magic #(80 50)) ;; P2 magic number
+           (%read-pgm-ascii-stream stream))
+          ((equalp magic #(80 53)) ;; P5 magic number
+           (%read-pgm-binary-stream stream))
+          (t (error "Invalid PGM Magic Number")))))
 
 (defun read-pgm-file (pathname)
   (with-open-file (stream pathname :direction :input :element-type '(unsigned-byte 8))
@@ -58,7 +83,30 @@
 (defun write-integer (int stream)
   (map nil (lambda (x) (write-byte (char-code x) stream)) (format nil "~D" int)))
 
-(defun write-pgm-stream (stream image)
+(defun %write-pgm-ascii-stream (stream image)
+  (map nil (lambda (x) (write-byte (char-code x) stream)) "P2")
+  (write-byte (char-code #\Newline) stream)
+  (with-image-bounds (height width)
+      image
+    (write-integer width stream)
+    (write-byte (char-code #\Space) stream)
+    (write-integer height stream)
+    (write-byte (char-code #\Newline) stream)
+    (typecase image
+      (8-bit-gray-image
+       (write-integer #xff stream))
+      (16-bit-gray-image
+       (write-integer #xffff stream)))
+    (write-byte (char-code #\Newline) stream)
+    (loop for i below height
+       do (loop for j below width
+             do 
+               (write-integer (pixel image i j) stream)
+               (unless (= j (1- width))
+                 (write-byte (char-code #\Space) stream)))
+         (write-byte (char-code #\Newline) stream))))
+
+(defun %write-pgm-binary-stream (stream image)
   (map nil (lambda (x) (write-byte (char-code x) stream)) "P5")
   (write-byte (char-code #\Newline) stream)
   (with-image-bounds (height width)
@@ -87,9 +135,15 @@
                        (write-byte (ash val -8) stream)
                        (write-byte (logand val #xff) stream)))))))))
 
-(defun write-pgm-file (pathname image)
+(defun write-pgm-stream (stream image &key (binary t))
+  (if binary
+      (%write-pgm-binary-stream stream image)
+      (%write-pgm-ascii-stream stream image)))
+
+(defun write-pgm-file (pathname image
+                       &key (binary t))
   (with-open-file (stream pathname :direction :output
                           :if-exists :supersede
                           :element-type '(unsigned-byte 8))
-    (write-pgm-stream stream image)
+    (write-pgm-stream stream image :binary binary)
     (truename pathname)))
