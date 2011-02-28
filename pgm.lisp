c1ec296a0d9f10195693f144da51514ac380e2eb
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-02-28 14:15:39 -0800

    remove read-digit-chars and simplify read-pgm-stream

--- pgm.lisp
+++ pgm.lisp
@@ -21,36 +21,10 @@
           (skip-line stream))
      finally (return c)))
 
-(defun read-digit-chars (stream)
-  (loop for c = (code-char (read-byte stream))
-     while (digit-char-p c)
-     collect c))
-
 (defun read-number (stream)
   (let ((first-char (read-byte-skipping-whitespace stream)))
     (map 'string #'code-char
          (cons first-char (read-until-whitespace stream)))))
-
-
-(defun read-8-bit-gray-pgm-data (stream height width)
-  (let ((img (make-8-bit-gray-image height width)))    
-    (loop for i below height
-       do 
-         (loop for j below width
-            do (setf (pixel img i j)
-                     (read-byte stream))))
-    img))
-
-(defun read-16-bit-gray-pgm-data (stream height width)
-  (let ((img (make-8-bit-gray-image height width)))    
-    (loop for i below height
-       do 
-         (loop for j below width
-            do (setf (pixel img i j)
-                     (+ (ash (read-byte stream) 8)
-                        (read-byte stream)))))
-    img))
-
 
 (defun read-pgm-stream (stream)
   (let ((magic (make-array 2 :element-type '(unsigned-byte 8))))
@@ -59,11 +33,22 @@
         (let ((width (parse-integer (read-number stream)))
               (height (parse-integer (read-number stream)))
               (maxgray (parse-integer (read-number stream))))
-          (let ((img
-                 (if (< maxgray 256)
-                     (read-8-bit-gray-pgm-data stream height width)
-                     (read-16-bit-gray-pgm-data stream height width))))
-            img))
+          (if (< maxgray 256)
+              (let ((img (make-8-bit-gray-image height width)))    
+                (loop for i below height
+                   do 
+                     (loop for j below width
+                        do (setf (pixel img i j)
+                                 (read-byte stream))))
+                img)
+              (let ((img (make-8-bit-gray-image height width)))    
+                (loop for i below height
+                   do 
+                     (loop for j below width
+                        do (setf (pixel img i j)
+                                 (+ (ash (read-byte stream) 8)
+                                    (read-byte stream)))))
+                img)))
         (error "Invalid PGM Magic Number"))))
 
 (defun read-pgm-file (pathname)
