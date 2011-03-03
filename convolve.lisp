8b3f3df417ef138f4418093b4605801e65d732de
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-03-03 09:06:11 -0800

    new convolve routine deals better with edge effects

--- convolve.lisp
+++ convolve.lisp
@@ -5,11 +5,14 @@
 
 (defun normalize-array (array)
   (let ((sum (coerce (sum array) 'double-float)))
-    (when (not (zerop sum))
-      (map-array (lambda (n) (/ n sum)) array))))
+    (if (zerop sum)
+        array
+        (map-array (lambda (n) (/ n sum)) array))))
+
 
 (defun discrete-convolve (u v)
   "Perform a discrete convolution of matrix u with matrix v"
+  (declare (optimize (debug 3)))
   (let ((fit-function
          (let ((type (array-element-type u)))
            (cond
@@ -26,58 +29,37 @@
              ((equal type '(unsigned-byte 32))
               (make-constrain-fn 0 #xffffffff))
              (t #'identity)))))
-    (with-image-bounds (ur uc channels) u
-      (with-image-bounds (vr vc) v
-        ;; need a new matrix z to hold the values of the convolved matrix
-        ;; dim z should be dim u + dim v - 1
-        (let ((zr (+ ur vr -1))
-              (zc (+ uc vc -1)))
-          (let ((z (make-array (apply #'list zr zc
-                                      (when channels (list channels)))
-                               :element-type (array-element-type u))))
-            (if channels
-                (loop for channel below channels
-                   do 
-                   (dotimes (i zr)
-                     (let ((ustartr (max 0 (- i vr -1)))
-                           (uendr (min (- ur 1) i))
-                           (vstartr (- vr (max (- vr i) 1))))
-                       (dotimes (j zc)
-                         (let ((ustartc (max 0 (- j vc -1)))
-                               (uendc (min (- uc 1) j))
-                               (vstartc (- vc (max (- vc j) 1)))
-                               (acc 0))
-                           (do ((urow ustartr (1+ urow))
-                                (vrow vstartr (1- vrow)))
-                               ((> urow uendr))
-                             (do ((ucol ustartc (1+ ucol))
-                                  (vcol vstartc (1- vcol)))
-                                 ((> ucol uendc))
-                               (let ((uv (aref u urow ucol channel))
-                                     (vv (aref v vrow vcol)))
-                                 (incf acc (* uv vv)))))
-                           (setf (aref z i j channel) (funcall fit-function acc)))))))
-                (dotimes (i zr)
-                  (let ((ustartr (max 0 (- i vr -1)))
-                        (uendr (min (- ur 1) i))
-                        (vstartr (- vr (max (- vr i) 1))))
-                    (dotimes (j zc)
-                      (let ((ustartc (max 0 (- j vc -1)))
-                            (uendc (min (- uc 1) j))
-                            (vstartc (- vc (max (- vc j) 1)))
-                            (acc 0))
-                        (do ((urow ustartr (1+ urow))
-                             (vrow vstartr (1- vrow)))
-                            ((> urow uendr))
-                          (do ((ucol ustartc (1+ ucol))
-                               (vcol vstartc (1- vcol)))
-                              ((> ucol uendc))
-                            (let ((uv (aref u urow ucol))
-                                  (vv (aref v vrow vcol)))
-                              (incf acc (* uv vv)))))
-                        (setf (aref z i j) (funcall fit-function acc)))))))
-            z))))))
-
+    (with-image-bounds (vr vc) v
+      (unless (and (= vr vc)
+                   (oddp vr))
+        (error "convolution matrix bust be square and of odd dimensions"))
+      (let ((span (ash vr -1)))
+        (with-image-bounds (ur uc channels) u
+          (let ((zr (+ ur vr -1))
+                (zc (+ uc vc -1)))
+            (let ((z (make-array (apply #'list zr zc
+                                        (when channels (list channels)))
+                                 :element-type (array-element-type u))))
+              (do-pixels (i j) z
+                (let ((acc (if channels
+                               (make-list channels :initial-element 0)
+                               0)))
+                  (loop for ui from (- i span) to (+ i span)
+                     for vi downfrom (1- vr) downto 0
+                     do (loop for uj from (- j span) to (+ j span)
+                           for vj downfrom (1- vc) downto 0
+                           do 
+                             (let ((ui* (constrain ui 0 (1- ur)))
+                                   (uj* (constrain uj 0 (1- uc))))
+                               (if channels
+                                   (setf acc (mapcar (lambda (a q) (+ a (* q (aref v vi vj))))
+                                                     acc
+                                                     (multiple-value-list (pixel u ui* uj*))))
+                                   (incf acc (* (pixel u ui* uj*) (aref v vi vj)))))))
+                  (if channels
+                      (setf (pixel z i j) (values-list (mapcar fit-function acc)))
+                      (setf (pixel z i j) (funcall fit-function acc)))))
+              z)))))))
 
 (defparameter *gaussian-kernel*
   (normalize-array #2A((1 2 1)
