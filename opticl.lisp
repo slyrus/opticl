090aedbffa10a433472a11667257c9678be43410
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-03-07 21:33:42 -0800

    wrap %get-image-dimensions in eval-when (:compiple-toplevel :load-toplevel :execute) so that it's available to the pixel setf-expander

--- opticl.lisp
+++ opticl.lisp
@@ -90,17 +90,18 @@
                `(frob-image ,name ,image-type ,element-type ,num-channels)))))
   (frobber))
 
-(defun %get-image-dimensions (image-var env)
-  #+(or sbcl ccl)
-  (when (symbolp image-var)
-    (multiple-value-bind (binding-type localp declarations)
-        (opticl-cltl2:variable-information image-var env)
-      (declare (ignore binding-type localp))
-      (let ((type-decl (find 'type declarations :key #'car)))
-        (and type-decl
-             (listp type-decl)
-             (= (length type-decl) 4)
-             (fourth type-decl))))))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun %get-image-dimensions (image-var env)
+    #+(or sbcl ccl)
+    (when (symbolp image-var)
+      (multiple-value-bind (binding-type localp declarations)
+          (opticl-cltl2:variable-information image-var env)
+        (declare (ignore binding-type localp))
+        (let ((type-decl (find 'type declarations :key #'car)))
+          (and type-decl
+               (listp type-decl)
+               (= (length type-decl) 4)
+               (fourth type-decl)))))))
 
 (defconstant +max-image-channels+ 4)
 
