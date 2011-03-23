7f2199c9121c9c5a3e3456f71a2f56ba090e00e8
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-03-23 09:37:48 -0700

    add fast discrete-convolve for 8-bit-rgb-array convolved with a double-float array, add &key element-type arg to normalize-array

--- convolve.lisp
+++ convolve.lisp
@@ -3,11 +3,13 @@
 
 (in-package :opticl)
 
-(defun normalize-array (array)
+(defun normalize-array (array &key element-type)
   (let ((sum (coerce (sum array) 'double-float)))
     (if (zerop sum)
         array
-        (map-array (lambda (n) (/ n sum)) array))))
+        (apply #'map-array (lambda (n) (/ n sum)) array
+               :force-simple t
+               (when element-type `(:element-type ,element-type))))))
 
 
 (defun discrete-convolve (u v)
@@ -33,37 +35,75 @@
                    (oddp vr))
         (error "convolution matrix bust be square and of odd dimensions"))
       (let ((span (ash vr -1)))
+        (declare (type fixnum span))
         (with-image-bounds (ur uc channels) u
           (let ((zr (+ ur vr -1))
                 (zc (+ uc vc -1)))
             (let ((z (make-array (apply #'list zr zc
                                         (when channels (list channels)))
                                  :element-type (array-element-type u))))
-              (do-pixels (i j) z
-                (let ((acc (if channels
-                               (make-list channels :initial-element 0)
-                               0)))
-                  (loop for ui from (- i span) to (+ i span)
-                     for vi downfrom (1- vr) downto 0
-                     do (loop for uj from (- j span) to (+ j span)
-                           for vj downfrom (1- vc) downto 0
-                           do 
-                             (let ((ui* (constrain ui 0 (1- ur)))
-                                   (uj* (constrain uj 0 (1- uc))))
-                               (if channels
-                                   (setf acc (mapcar (lambda (a q) (+ a (* q (aref v vi vj))))
-                                                     acc
-                                                     (multiple-value-list (pixel u ui* uj*))))
-                                   (incf acc (* (pixel u ui* uj*) (aref v vi vj)))))))
-                  (if channels
-                      (setf (pixel z i j) (values-list (mapcar fit-function acc)))
-                      (setf (pixel z i j) (funcall fit-function acc)))))
+              ;; let's speed this up for some common cases:
+              (cond ((and (typep u '8-bit-rgb-image)
+                          (typep v 'double-float-gray-image))
+                     (locally
+                         (declare (type 8-bit-rgb-image u z)
+                                  (type double-float-gray-image v)
+                                  (type fixnum span)
+                                  (optimize (speed 3)))
+                       (do-pixels (i j) z
+                         (let ((acc-r 0d0) (acc-g 0d0) (acc-b 0d0))
+                           (declare (type double-float acc-r acc-g acc-b ))
+                           (loop for ui fixnum from (- i span) to (+ i span)
+                              for vi fixnum downfrom (1- vr) downto 0
+                              do (loop for uj fixnum from (- j span) to (+ j span)
+                                    for vj fixnum downfrom (1- vc) downto 0
+                                    do 
+                                      (let ((ui* (constrain ui 0 (1- ur)))
+                                            (uj* (constrain uj 0 (1- uc)))
+                                            (v-val (aref v vi vj)))
+                                        (declare (type double-float v-val))
+                                        (multiple-value-bind (r g b) (pixel u ui* uj*)
+                                          (declare (type (unsigned-byte 8) r g b))
+                                          (setf acc-r (+ acc-r (* r v-val)))
+                                          (setf acc-g (+ acc-g (* g v-val)))
+                                          (setf acc-b (+ acc-b (* b v-val)))))))
+                           (setf (pixel z i j) (values 
+                                                (cond ((<= acc-r 0d0) 0)
+                                                      ((>= acc-r 255d0) 255)
+                                                      (t (round acc-r)))
+                                                (cond ((<= acc-g 0d0) 0)
+                                                      ((>= acc-g 255d0) 255)
+                                                      (t (round acc-g)))
+                                                (cond ((<= acc-b 0d0) 0)
+                                                      ((>= acc-b 255d0) 255)
+                                                      (t (round acc-b)))))))))
+                    (t
+                     (do-pixels (i j) z
+                       (let ((acc (if channels
+                                      (make-list channels :initial-element 0)
+                                      0)))
+                         (loop for ui fixnum from (- i span) to (+ i span)
+                            for vi fixnum downfrom (1- vr) downto 0
+                            do (loop for uj fixnum from (- j span) to (+ j span)
+                                  for vj fixnum downfrom (1- vc) downto 0
+                                  do 
+                                  (let ((ui* (constrain ui 0 (1- ur)))
+                                        (uj* (constrain uj 0 (1- uc))))
+                                    (if channels
+                                        (setf acc (mapcar (lambda (a q) (+ a (* q (aref v vi vj))))
+                                                          acc
+                                                          (multiple-value-list (pixel u ui* uj*))))
+                                        (incf acc (* (pixel u ui* uj*) (aref v vi vj)))))))
+                         (if channels
+                             (setf (pixel z i j) (values-list (mapcar fit-function acc)))
+                             (setf (pixel z i j) (funcall fit-function acc)))))))
               z)))))))
 
 (defparameter *gaussian-kernel*
   (normalize-array #2A((1 2 1)
                        (2 4 2)
-                       (1 2 1))))
+                       (1 2 1))
+                   :element-type 'double-float))
 (defun blur-image (img)
   (trim-image
    (discrete-convolve img *gaussian-kernel*) 1 1))
