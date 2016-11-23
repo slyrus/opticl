001b1436b6427aeae6d1ccf9c44e8a7c08ba87b2
Cyrus Harmon <cyrus@bobobeach.com>
2016-11-23 09:38:07 -0800

    add more type declarations in TIFF and coerce routines
     * removes almost all of the compilation notes

--- coerce.lisp
+++ coerce.lisp
@@ -174,56 +174,64 @@
   (declare (ignore args))
   (etypecase image
     (gray-image
-     (with-image-bounds (y x)
-         image
-       (let* ((type (array-element-type image))
-              (rgb-image (make-array (list y x 3) :element-type type)))
-         (declare (type rgb-image rgb-image))
-         (do-pixels (i j)
-             image
-           (let ((val (pixel image i j)))
-             (setf (pixel rgb-image i j) 
-                   (values val val val))))
-         rgb-image)))
+     (locally
+         (declare (type gray-image image))
+       (with-image-bounds (y x)
+           image
+         (let* ((type (array-element-type image))
+                (rgb-image (make-array (list y x 3) :element-type type)))
+           (declare (type rgb-image rgb-image))
+           (do-pixels (i j)
+               image
+             (let ((val (pixel image i j)))
+               (setf (pixel rgb-image i j)
+                     (values val val val))))
+           rgb-image))))
     (rgb-image image)
     (rgba-image
-     (with-image-bounds (y x channels)
-         image
-       (let* ((type (array-element-type image))
-              (rgb-image (make-array (list y x 3) :element-type type)))
-         (do-pixels (i j)
-             image
-           (setf (pixel rgb-image i j) 
-                 (pixel image i j)))
-         rgb-image)))))
+     (locally
+         (declare (type rgba-image image))
+       (with-image-bounds (y x channels)
+           image
+         (let* ((type (array-element-type image))
+                (rgb-image (make-array (list y x 3) :element-type type)))
+           (do-pixels (i j)
+               image
+             (setf (pixel rgb-image i j)
+                   (pixel image i j)))
+           rgb-image))))))
 
 (defmethod coerce-image (image (type (eql '8-bit-rgba-image)) &rest args)
   (declare (ignore args))
   (etypecase image
     (gray-image
-     (with-image-bounds (y x)
-         image
-       (let* ((type (array-element-type image))
-              (rgba-image (make-array (list y x 4) :element-type type)))
-         (declare (type rgba-image rgba-image))
-         (do-pixels (i j)
-             image
-           (let ((val (pixel image i j)))
-             (setf (pixel rgba-image i j) 
-                   (values val val val 255))))
-         rgba-image)))
-    (rgb-image 
-     (with-image-bounds (y x channels)
-         image
-       (let* ((type (array-element-type image))
-              (rgba-image (make-array (list y x 4) :element-type type)))
-         (declare (type rgba-image rgba-image))
-         (do-pixels (i j)
-             image
-           (setf (pixel* rgba-image i j) 
-                 (append (multiple-value-list (pixel image i j))
-                         (list 255))))
-         rgba-image)))
+     (locally
+         (declare (type gray-image image))
+       (with-image-bounds (y x)
+           image
+         (let* ((type (array-element-type image))
+                (rgba-image (make-array (list y x 4) :element-type type)))
+           (declare (type rgba-image rgba-image))
+           (do-pixels (i j)
+               image
+             (let ((val (pixel image i j)))
+               (setf (pixel rgba-image i j)
+                     (values val val val 255))))
+           rgba-image))))
+    (rgb-image
+     (locally
+         (declare (type rgb-image image))
+       (with-image-bounds (y x channels)
+           image
+         (let* ((type (array-element-type image))
+                (rgba-image (make-array (list y x 4) :element-type type)))
+           (declare (type rgba-image rgba-image))
+           (do-pixels (i j)
+               image
+             (setf (pixel* rgba-image i j)
+                   (append (multiple-value-list (pixel image i j))
+                           (list 255))))
+           rgba-image))))
     (rgba-image image)))
 
 (defmethod coerce-image (image (type (eql 'rgba-image)) &rest args)
